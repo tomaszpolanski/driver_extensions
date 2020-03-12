a50f262fb638a7f114bfcf7093afbb3ac4e07bbf
Tomek Polanski <tpolanski@groupon.com>
2020-03-12 09:54:38 +0100

    Exposing currentTestFile

--- lib/driver_extensions.dart
+++ lib/driver_extensions.dart
@@ -1,3 +1,4 @@
 library driver_extensions;
 
 export 'src/extensions.dart';
+export 'src/stack_trace.dart';
--- test/stack_trace_test.dart
+++ test/stack_trace_test.dart
@@ -5,13 +5,13 @@ import 'package:stack_trace/stack_trace.dart';
 void main() {
   group('testFileName', () {
     test('returns file name', () {
-      final result = testFileName(StackTrace.current);
+      final result = currentTestFile(StackTrace.current);
 
       expect(result, startsWith('stack_trace_test.dart'));
     });
 
     test('return null if not found', () {
-      final result = testFileName(Chain([]));
+      final result = currentTestFile(Chain([]));
 
       expect(result, isNull);
     });
