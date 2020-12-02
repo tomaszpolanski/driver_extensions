38935da9430e6d886fdb6d16bc86cc724a8b81ad
Tomek Polański <polanski.tomek@gmail.com>
2020-12-02 21:08:41 +0100

    Temporarily removing circular dependencies

--- test_driver/generic/test_app/routes.dart
+++
@@ -1,5 +0,0 @@
-const tapping_page = '/tapping';
-const long_pressing_page = '/long_pressing';
-const horizontal_swiping_page = '/horizontal_swipe';
-const vertical_swiping_page = '/vertical_swiping';
-const page5 = '/page5';
